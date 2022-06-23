73658e5940d1938ea01c299bcb430559ea422ebf
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-06-23 19:58:30 +0300

    initial SwiftUIAsyncImagePractice

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Kirill Sytkov on 23.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
