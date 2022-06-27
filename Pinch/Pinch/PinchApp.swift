ee7e4df6f6051e1e019eccab45c339ba1f01b96c
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-06-27 12:18:55 +0300

    initial Pinch application

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Kirill Sytkov on 27.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Kirill Sytkov on 27.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
