892419985a69025107f67f4fc22bc07533bb78b9
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-06-29 17:50:47 +0300

    initial Fructus application

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Kirill Sytkov on 29.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fructus/Fructus/Screens/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Kirill Sytkov on 29.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            
+    }
+}
