fe67f2bdfe7a9c138077e07d89cb5dbdb03aff22
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-07-01 16:03:19 +0300

    feat: add Fruit detail view

--- Fructus/Fructus/Screens/ContentView.swift
+++ Fructus/Fructus/Screens/ContentView.swift
@@ -8,28 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    //MARK: - Properties
-    var fruits: [Fruit] = fruitsData
-    
-    //MARK: - Body
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(fruits.shuffled()) { fruit in
-                    FruitRowView(fruit: fruit)
-                        .padding(.vertical, 4)
-                }
-            }
-            .navigationTitle("Fruits")
-        } // Navigation
-
-    }
+   //MARK: - Properties
+   var fruits: [Fruit] = fruitsData
+   
+   //MARK: - Body
+   var body: some View {
+      NavigationView {
+         List {
+           ForEach(fruits.shuffled()) { item in
+             NavigationLink(destination: FruitDetailView(fruit: item)) {
+               FruitRowView(fruit: item)
+                 .padding(.vertical, 4)
+             }
+           }
+         }
+         .navigationTitle("Fruits")
+      } // Navigation
+      .navigationViewStyle(StackNavigationViewStyle())
+   }
 }
 
 //MARK: - Preview
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView(fruits: fruitsData)
-            
-    }
+   static var previews: some View {
+      ContentView(fruits: fruitsData)
+      
+   }
 }
---
+++ Fructus/Fructus/Views/FruitHeaderView.swift
@@ -0,0 +1,40 @@
+//
+//  FruitHeaderView.swift
+//  Fructus
+//
+//  Created by Kirill Sytkov on 01.07.2022.
+//
+
+import SwiftUI
+
+struct FruitHeaderView: View {
+    //MARK: - Properties
+    @State private var isAnimating = false
+    
+    var fruit: Fruit
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .topLeading, endPoint: .bottomTrailing)
+            Image(fruit.image)
+                .resizable()
+                .scaledToFit()
+                .shadow(color: .black.opacity(0.15), radius: 8, x: 6, y: 8)
+                .padding(.vertical, 20)
+                .scaleEffect(isAnimating ? 1 : 0.6)
+        } // ZSTACK
+        .frame(height: 440)
+        .onAppear() {
+            withAnimation(.easeOut(duration: 0.5)) {
+                isAnimating = true
+            }
+        }
+    }
+}
+
+struct FruitHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitHeaderView(fruit: fruitsData[0])
+            .previewLayout(.fixed(width: 375, height: 440))
+    }
+}
