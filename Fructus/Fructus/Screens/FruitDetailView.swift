327049c8e719245cb22763eb8860834adc563c47
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-07-02 15:20:32 +0300

    feat: add fruit nutritional view

--- Fructus/Fructus/Screens/FruitDetailView.swift
+++ Fructus/Fructus/Screens/FruitDetailView.swift
@@ -30,7 +30,7 @@ struct FruitDetailView: View {
                             .font(.headline)
                             .multilineTextAlignment(.leading)
                         //Nutrients
-                        
+                        FruitNutrientsView(fruit: fruit)
                         // Subheadline
                         Text("Learn more about \(fruit.title)".uppercased())
                             .fontWeight(.bold)
