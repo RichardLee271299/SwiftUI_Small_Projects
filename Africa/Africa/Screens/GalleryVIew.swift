3f6ef8577ee0ba19347b583f67ef6e9d46ec2890
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-07-05 17:21:47 +0300

    initial new app

---
+++ Africa/Africa/Screens/GalleryVIew.swift
@@ -0,0 +1,20 @@
+//
+//  GalleryVIew.swift
+//  Africa
+//
+//  Created by Kirill Sytkov on 05.07.2022.
+//
+
+import SwiftUI
+
+struct GalleryVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GalleryVIew_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryVIew()
+    }
+}
