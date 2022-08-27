6a177160d93609ced0ff27a1b6b8de46c7b05e8b
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-08-27 20:57:59 +0300

    feat: update UI and add some logic to save the tasks

---
+++ Devote/Devote/Utility/Constant.swift
@@ -0,0 +1,23 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Kirill Sytkov on 27.08.2022.
+//
+
+import SwiftUI
+
+//MARK: - Formatter
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+
+//MARK: - UI
+
+
+//MARK: - UX
--- Devote/Devote/View/ContentView.swift
+++ Devote/Devote/View/ContentView.swift
@@ -9,80 +9,118 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        NavigationView {
+   //MARK: - Properties
+   @State var task: String = ""
+   
+   private var isButtonDisabled: Bool {
+      task.isEmpty
+   }
+   
+   //Fetching data
+   @Environment(\.managedObjectContext) private var viewContext
+   
+   @FetchRequest(
+      sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+      animation: .default)
+   private var items: FetchedResults<Item>
+   
+   //MARK: - Body
+   var body: some View {
+      NavigationView {
+         VStack {
+            VStack(spacing: 16) {
+               TextField("New Task", text: $task)
+                  .padding()
+                  .background(
+                     Color(UIColor.systemGray6)
+                  )
+                  .cornerRadius(10)
+               Button {
+                  addItem()
+               } label: {
+                  Spacer()
+                  Text("Save")
+                  Spacer()
+               }
+               .disabled(isButtonDisabled)
+               .padding()
+               .font(.headline)
+               .foregroundColor(.white)
+               .background(isButtonDisabled ? Color.gray : Color.pink)
+               .cornerRadius(10)
+            }//VSTACK
+            .padding()
+            
             List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
+               ForEach(items) { item in
+                  NavigationLink {
+                     Text(item.task ?? "")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                     Text(item.timestamp!, formatter: itemFormatter)
+                        .font(.footnote)
+                     .foregroundColor(.gray)
+                  } label: {
+                     VStack(alignment: .leading) {
+                        Text(item.task ?? "")
+                           .font(.headline)
+                           .fontWeight(.bold)
                         Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
+                           .font(.footnote)
+                        .foregroundColor(.gray)
+                     }
+                  }
+               }
+               .onDelete(perform: deleteItems)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
+         } // VStack
+         .navigationTitle("Daily Tasks")
+         .navigationBarTitleDisplayMode(.large)
+         .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+               EditButton()
             }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+         }// Toolbar
+         Text("Select an item")
+      }// Navigation
+   }
+   
+   //MARK: - Func
+   private func addItem() {
+      withAnimation {
+         let newItem = Item(context: viewContext)
+         newItem.timestamp = Date()
+         newItem.task = task
+         newItem.completion = false
+         newItem.id = UUID()
+         do {
+            try viewContext.save()
+         } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+         }
+         task = ""
+         hideKeyboard()
+      }
+   }
+   
+   private func deleteItems(offsets: IndexSet) {
+      withAnimation {
+         offsets.map { items[$0] }.forEach(viewContext.delete)
+         
+         do {
+            try viewContext.save()
+         } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+         }
+      }
+   }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
+//MARK: - Preview
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
+   static var previews: some View {
+      ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+   }
 }
