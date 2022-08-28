23bf8232f757dc0025138f1162d979b0bdb3d0aa
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-08-28 12:46:48 +0300

    feat: add gradient and background to content view

--- Devote/Devote/View/ContentView.swift
+++ Devote/Devote/View/ContentView.swift
@@ -27,53 +27,62 @@ struct ContentView: View {
    //MARK: - Body
    var body: some View {
       NavigationView {
-         VStack {
-            VStack(spacing: 16) {
-               TextField("New Task", text: $task)
-                  .padding()
-                  .background(
-                     Color(UIColor.systemGray6)
-                  )
-                  .cornerRadius(10)
-               Button {
-                  addItem()
-               } label: {
-                  Spacer()
-                  Text("Save")
-                  Spacer()
-               }
-               .disabled(isButtonDisabled)
-               .padding()
-               .font(.headline)
-               .foregroundColor(.white)
-               .background(isButtonDisabled ? Color.gray : Color.pink)
-               .cornerRadius(10)
-            }//VSTACK
-            .padding()
-            
-            List {
-               ForEach(items) { item in
-                  NavigationLink {
-                     Text(item.task ?? "")
-                        .font(.headline)
-                        .fontWeight(.bold)
-                     Text(item.timestamp!, formatter: itemFormatter)
-                        .font(.footnote)
-                     .foregroundColor(.gray)
+         ZStack {
+            VStack {
+               VStack(spacing: 16) {
+                  TextField("New Task", text: $task)
+                     .padding()
+                     .background(
+                        Color(UIColor.systemGray6)
+                     )
+                     .cornerRadius(10)
+                  Button {
+                     addItem()
                   } label: {
-                     VStack(alignment: .leading) {
+                     Spacer()
+                     Text("Save")
+                     Spacer()
+                  }
+                  .disabled(isButtonDisabled)
+                  .padding()
+                  .font(.headline)
+                  .foregroundColor(.white)
+                  .background(isButtonDisabled ? Color.gray : Color.pink)
+                  .cornerRadius(10)
+               }//VSTACK
+               .padding()
+               
+               List {
+                  ForEach(items) { item in
+                     NavigationLink {
                         Text(item.task ?? "")
                            .font(.headline)
                            .fontWeight(.bold)
                         Text(item.timestamp!, formatter: itemFormatter)
                            .font(.footnote)
-                        .foregroundColor(.gray)
+                           .foregroundColor(.gray)
+                     } label: {
+                        VStack(alignment: .leading) {
+                           Text(item.task ?? "")
+                              .font(.headline)
+                              .fontWeight(.bold)
+                           Text(item.timestamp!, formatter: itemFormatter)
+                              .font(.footnote)
+                              .foregroundColor(.gray)
+                        }
                      }
                   }
-               }
-               .onDelete(perform: deleteItems)
-            }
-         } // VStack
+                  .onDelete(perform: deleteItems)
+               } //List
+               .listStyle(InsetGroupedListStyle())
+               .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 12)
+               .padding(.vertical, 0)
+               .frame(maxWidth: 640)
+            } // VStack
+         }//ZStack
+         .onAppear() {
+            UITableView.appearance().backgroundColor = UIColor.clear
+         }
          .navigationTitle("Daily Tasks")
          .navigationBarTitleDisplayMode(.large)
          .toolbar {
@@ -81,8 +90,14 @@ struct ContentView: View {
                EditButton()
             }
          }// Toolbar
-         Text("Select an item")
+         .background(
+            BackgroundImageView()
+         )
+         .background(
+            backgroundGradient.ignoresSafeArea(.all)
+         )
       }// Navigation
+      .navigationViewStyle(StackNavigationViewStyle())
    }
    
    //MARK: - Func
