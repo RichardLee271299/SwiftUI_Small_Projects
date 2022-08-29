a9e221a32c3487e5ff9c59db45a799d2d2786e6b
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-08-29 21:07:00 +0300

    initial Notes project for appWatch

---
+++ Notes/Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Kirill Sytkov on 29.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
