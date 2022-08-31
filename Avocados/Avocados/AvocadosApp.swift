7be58b90fbbfaf69dfffaf412328895560a24c15
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-08-31 19:12:08 +0300

    initial project Avocados

---
+++ Avocados/Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Kirill Sytkov on 31.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
