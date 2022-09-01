d469be78891a78d5e743afe5c2240688c76e5904
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-09-01 12:58:18 +0300

    feat: create avocados view

--- Avocados/Avocados/View/AvocadosView.swift
+++ Avocados/Avocados/View/AvocadosView.swift
@@ -8,13 +8,57 @@
 import SwiftUI
 
 struct AvocadosView: View {
+   //MARK: - Properties
+   @State private var pulsateAnimation = false
+   
+   
+   //MARK: - Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       VStack {
+          Spacer()
+          Image("avocado")
+             .resizable()
+             .aspectRatio(contentMode: .fit)
+             .frame(width: 240, height: 240, alignment: .center)
+             .shadow(color: Color("ColorBlackTransparentDark"), radius: 12, x: 0, y: 8)
+             .scaleEffect(self.pulsateAnimation ? 1 : 0.9)
+             .opacity(self.pulsateAnimation ? 1 :0.9)
+             .animation(.easeOut(duration: 1.5).repeatForever(autoreverses: true),value: pulsateAnimation)
+             
+          VStack {
+             Text("Avocados".uppercased()
+             )
+                .font(.system(size: 42, weight: .bold, design: .serif))
+                .foregroundColor(.white)
+                .padding()
+             .shadow(color: Color("ColorBlackTransparentDark"), radius: 4, x: 0, y: 4)
+             Text("Creamy, green, and full of nutrients! Avocado is a powerhouse ingredient at any meal. Enjoy these handpicked avocado recipes for breakfast, lunch, dinner & more!")
+                .lineLimit(nil)
+                .font(.system(.headline, design: .serif))
+                .foregroundColor(Color("ColorGreenLight"))
+                .multilineTextAlignment(.center)
+                .lineSpacing(8)
+                .frame(maxWidth: 640, maxHeight: 120)
+          }
+          .padding()
+          
+          Spacer()
+       }
+       .background(
+       Image("background")
+         .resizable()
+         .aspectRatio(contentMode: .fill)
+       )
+       .edgesIgnoringSafeArea(.all)
+       .onAppear {
+          self.pulsateAnimation.toggle()
+       }
     }
 }
 
 struct AvocadosView_Previews: PreviewProvider {
     static var previews: some View {
-        AvocadosView()
+       AvocadosView()
+          
     }
 }
