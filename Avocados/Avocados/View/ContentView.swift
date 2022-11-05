256cb6f436d8a6e0e0adfac3a661cb08f7be10e7
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-11-05 21:25:40 +0300

    feat: add dishes view

--- Avocados/Avocados/View/ContentView.swift
+++ Avocados/Avocados/View/ContentView.swift
@@ -23,20 +23,24 @@ struct ContentView: View {
                     }
                 }
                
+                //MARK: - Dishes
+                Text("Avocado Dishes")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
                 
+                DishesView()
+                    .frame(maxWidth:640)
                 
                 //MARK: - Footer
                 VStack(alignment: .center, spacing: 20) {
                     Text("All About Avocados")
-                        .font(.system(.title, design: .serif))
                         .fontWeight(.bold)
-                        .foregroundColor(Color("ColorGreenAdaptive"))
-                        .padding(8)
+                        .modifier(TitleModifier())
                     Text("Everything you wanted to know about avocados but weew too afraid to ask.")
                         .font(.system(.body, design: .serif))
                         .multilineTextAlignment(.center)
                         .foregroundColor(.gray)
-                    
+                        .frame(minHeight: 60)
                 }
                 .frame(maxWidth: 640)
                 .padding()
@@ -45,6 +49,15 @@ struct ContentView: View {
         }
         .edgesIgnoringSafeArea(.all)
         .padding(0)
+    }
+}
+
+struct TitleModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+        .font(.system(.title, design: .serif))
+        .foregroundColor(Color("ColorGreenAdaptive"))
+        .padding(8)
     }
 }
 
