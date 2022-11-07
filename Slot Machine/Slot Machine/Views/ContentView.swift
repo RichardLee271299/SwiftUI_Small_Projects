17d89b71e996bd8c5926735045fe0baaa9979111
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-11-07 16:22:20 +0300

    initial project Slot machine

---
+++ Slot Machine/Slot Machine/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slot Machine
+//
+//  Created by Kirill Sytkov on 07.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
