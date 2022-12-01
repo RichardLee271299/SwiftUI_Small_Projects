1a02461f7af60d25194cb85c727ea40e0b41e067
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-12-01 20:45:01 +0300

    feat: add func to create and delete task

--- Todo/Todo/View/ContentView.swift
+++ Todo/Todo/View/ContentView.swift
@@ -12,13 +12,24 @@ struct ContentView: View {
     
     @Environment(\.managedObjectContext) var managedObjectContext
     
+    @FetchRequest(entity: TodoItem.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \TodoItem.name, ascending: true)]) var todos: FetchedResults<TodoItem>
+    
     @State private var showingAddTodoView = false
     
     //MARK: - Body
     var body: some View {
         NavigationView {
-            List(0..<5) { item in
-                Text("hi")
+            List {
+                ForEach(self.todos, id: \.self) { todo in
+                    HStack {
+                        Text(todo.name ?? "Unknown")
+                        
+                        Spacer()
+                        
+                        Text(todo.priority ?? "Unknown")
+                    }
+                }//: FOREACH
+                .onDelete(perform: deleteTodo(at:))
             } //: LIST
             .navigationTitle("Todo")
             .navigationBarTitleDisplayMode(.inline)
@@ -33,8 +44,25 @@ struct ContentView: View {
                         AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
                     }
                 }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
             }
         }//:NAVIGATION
+    }
+    
+    //MARK: - Funcs
+    private func deleteTodo(at offsets: IndexSet) {
+        for index in offsets {
+            let todo = todos[index]
+            managedObjectContext.delete(todo)
+            
+            do {
+                try managedObjectContext.save()
+            } catch {
+                print(error)
+            }
+        }
     }
 }
 
