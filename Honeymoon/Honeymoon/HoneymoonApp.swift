63c2310833e8d310f3b0291f80b73b38348854af
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-12-09 19:27:55 +0300

    initial new project

---
+++ Honeymoon/Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Kirill Sytkov on 09.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
