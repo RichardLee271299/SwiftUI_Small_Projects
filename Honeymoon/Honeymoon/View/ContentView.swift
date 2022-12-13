f80b1af01ee19469560ae97695de15a16948b0c9
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-12-13 21:23:26 +0300

    feat: add cards stack view

--- Honeymoon/Honeymoon/View/ContentView.swift
+++ Honeymoon/Honeymoon/View/ContentView.swift
@@ -13,23 +13,46 @@ struct ContentView: View {
     @State var showGuideView = false
     @State var showInfoView = false
     
+    var cardViews: [CardView] = {
+        var views = [CardView]()
+        for index in 0..<2  {
+            views.append(CardView(honeymoon: honeymoonData[index]))
+        }
+        return views
+    }()
+    
     //MARK: - Body
     var body: some View {
         VStack {
+            //MARK: - Header
             HeaderView(showGuideView: $showGuideView,showInfoView: $showInfoView)
             
             Spacer()
-            
-            CardView(honeymoon: honeymoonData[0])
-                .padding()
+            //MARK: - Cards
+            ZStack {
+                ForEach(cardViews) { cardView in
+                    cardView
+                        .zIndex(self.isTopCard(cardView: cardView) ? 1 : 0)
+                }
+            }
+            .padding(.horizontal)
             
             Spacer()
-            
+            //MARK: - Footer
             FooterView(showBookingAlert: $showAlert)
         }
         .alert(isPresented: $showAlert) {
             Alert(title: Text("SUCCESS"), message: Text("Wishing a lovely and most precious of the times together for the amazing couple."), dismissButton: .default(Text("Happy Honeymoon!")))
         }
+    }
+    
+    
+    //MARK: - funcs
+    private func isTopCard(cardView: CardView) -> Bool {
+        guard let index = cardViews.firstIndex(where: { $0.id == cardView.id}) else {
+            return false
+        }
+        return index == 0
     }
 }
 
